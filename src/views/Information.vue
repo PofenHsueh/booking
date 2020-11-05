124f7e221af58cc50a6aa865c2342bc1a09a7876
jade.hsueh <jade.hsueh@belstar.com.cn>
2020-11-05 15:11:40 +0800

    add text-field css

--- src/views/Information.vue
+++ src/views/Information.vue
@@ -2,43 +2,49 @@
   <div class="info">
     <v-col cols="3" class="info--persional">
       <h1>Reservation<br />Information</h1>
-      <div v-for="(item, index) in infoData" :key="index">
-        <v-icon>mdi-{{ item.icon }}</v-icon>
+      <div
+        v-for="(item, index) in infoData"
+        :key="index"
+        :class="getColor(index, item.change)"
+      >
+        <v-icon :class="getColor(index, item.change)"
+          >mdi-{{ item.icon }}</v-icon
+        >
         <p class="display-inline">
           <strong>{{ item.title }}</strong>
         </p>
-        <v-text-field></v-text-field>
+        <v-text-field
+          color="#569dc3"
+          @focus="item.change = true"
+          @blur="item.change = false"
+        ></v-text-field>
       </div>
       <v-btn width="100%" depressed height="60" dark color="#569DC3" tile
         >RESERVE NOW</v-btn
       >
     </v-col>
-    <v-col cols="3">
-      <v-card class="info--type">
+    <v-col cols="3" style="margin:42px">
+      <v-card class="info--type" flat outlined>
         <v-img :src="getImg($route.query.from)" aspect-ratio="1.71"></v-img>
         <h2>{{ $route.query.from }}</h2>
         <hr />
-        <div
-          v-for="(type, index) in types"
-          :key="index"
-          class="info--type--date"
-        >
-          <v-icon>mdi-calendar-range</v-icon>
-          <h3>{{ type.title }}</h3>
-          <p>{{ type.date }}</p>
-          <hr />
-        </div>
-        <section v-for="money in total" :key="money.price">
-          <p>{{ money.price }}</p>
-          <p>
-            <strong>{{ money.amount }}</strong>
-          </p>
-        </section>
-        <hr />
-        <section>
-          <p>TOTAL</p>
-          <p><strong>$10,988</strong></p>
-        </section>
+        <v-list>
+          <v-list-item v-for="(type, index) in types" :key="index">
+            <v-icon>mdi-calendar-range</v-icon>
+            <h3>{{ type.title }}</h3>
+            <p class="list__position">{{ type.date }}</p>
+          </v-list-item>
+          <v-list-item v-for="money in total" :key="money.price">
+            <p>{{ money.price }}</p>
+            <p class="list__position">
+              {{ money.amount }}
+            </p>
+          </v-list-item>
+          <v-list-item>
+            <p>TOTAL</p>
+            <p class="list__position"><strong>$10,988</strong></p>
+          </v-list-item>
+        </v-list>
       </v-card>
     </v-col>
   </div>
@@ -48,8 +54,8 @@ export default {
   data() {
     return {
       infoData: [
-        { title: "NAME", icon: "account" },
-        { title: "PHONE", icon: "cellphone-iphone" }
+        { title: "NAME", icon: "account", change: false },
+        { title: "PHONE", icon: "cellphone-iphone", change: false }
       ],
       types: [
         { title: "CHECK-IN", date: "2020-11-04" },
@@ -66,6 +72,11 @@ export default {
     getImg(type) {
       let images = require.context("../assets/img/", false, /\.jpeg$/);
       return images("./" + type + ".jpeg");
+    },
+    getColor(index, change) {
+      if (change) {
+        return "info--persional__focus" + "-" + index;
+      }
     }
   }
 };
@@ -78,20 +89,26 @@ export default {
   display: flex;
   justify-content: center;
   &--persional {
-    padding: 20px;
+    padding: 42px 20px 20px 20px;
     margin: 0px 85px;
+    h1 {
+      margin-bottom: 15px;
+    }
   }
   &--type {
     h2,
     section {
       padding: 15px;
     }
-    // &--date{
-    //   padding: 15px 0px;
-    //   .v-icon{
-    //     padding: 0px 15px;
-    //   }
-    // }
   }
+}
+.v-list-item {
+  border-bottom: 1px solid grey;
+  &:nth-last-child(1) {
+    border-bottom: none;
+  }
+}
+.list__position {
+  margin-left: auto;
 }
 </style>
